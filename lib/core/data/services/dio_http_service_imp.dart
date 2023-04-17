de4ce0a76a9ebf756112d5d29b4cea01b80b4de9
Deivid Willyan <deiviid_@hotmail.com>
2023-04-17 09:50:33 -0300

    Update dio_http_service_imp.dart

--- lib/core/data/services/dio_http_service_imp.dart
+++ lib/core/data/services/dio_http_service_imp.dart
@@ -10,7 +10,7 @@ class DioHttpServiceImp implements HttpService {
         headers: {
           'content-type': 'application/json;charset=utf-8',
           'authorization':
-              'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkODVhZjhlZDA0NTZhNWQyNzVmZmQxODI4YmJkYzY4NSIsInN1YiI6IjU5ODA1NjQ0YzNhMzY4MTA1NTAwZDRiNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MJcPKVkaqXdI_Oblbk-VjBM8pWtTmKltfxZqyuLIU_U',
+              'Bearer ',
         },
       ),
     );
